b34c3da5993db0ae0398cf1da1da6e9666a3fd48
Philip Cox <neogeneva@gmail.com>
2022-05-25 16:11:48 +1200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UseEndpointsNullArgumentException.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
+}
